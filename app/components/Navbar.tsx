4889c3529507b691511e40f848406c7137c3c271
boukhrisssaber <boukhrisssaber@gmail.com>
2025-07-10 21:20:05 +0100

    Rearranged the order of the pages in the NavBar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -36,17 +36,6 @@ export default function Navbar() {
                     Home
                   </Link>
                   <Link
-                    href="/guestbook"
-                    prefetch
-                    className={`${
-                      pathname === "/guestbook"
-                        ? "border-teal-500 dark:text-white h-full inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                        : "border-transparent text-gray-500 dark:text-gray-300 dark:hover:text-white inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                    }`}
-                  >
-                    Guestbook
-                  </Link>
-                  <Link
                     href="/projects"
                     prefetch
                     className={`${
@@ -67,6 +56,17 @@ export default function Navbar() {
                     }`}
                   >
                     Hire Me
+                  </Link>
+                  <Link
+                    href="/guestbook"
+                    prefetch
+                    className={`${
+                      pathname === "/guestbook"
+                        ? "border-teal-500 dark:text-white h-full inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                        : "border-transparent text-gray-500 dark:text-gray-300 dark:hover:text-white inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                    }`}
+                  >
+                    Guestbook
                   </Link>
                   <Themebutton />
                 </div>
