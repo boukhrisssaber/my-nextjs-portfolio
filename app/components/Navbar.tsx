c96cd6f6babd272254f92578bcae09e6a07aa9ab
boukhrisssaber <boukhrisssaber@gmail.com>
2025-11-04 10:50:25 +0100

    Removed Hire me page and did some content tweaks

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -68,17 +68,6 @@ export default function Navbar() {
                   >
                     Guestbook
                   </Link>
-                  <Link
-                    href="/hire-me"
-                    prefetch
-                    className={`${
-                      pathname === "/hire-me"
-                        ? "border-teal-500 dark:text-white h-full inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                        : "border-transparent text-gray-500 dark:text-gray-300 dark:hover:text-white inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                    }`}
-                  >
-                    Hire Me
-                  </Link>
                   <Themebutton />
                 </div>
               </div>
@@ -156,17 +145,6 @@ export default function Navbar() {
                 } `}
               >
                 Projects
-              </Link>
-              <Link
-                href="/hire-me"
-                prefetch
-                className={`${
-                  pathname == "/hire-me"
-                    ? "bg-teal-50 border-teal-500 text-teal-500 block pl-3 pr-4 py-2 border-l-4 text-base font-medium dark:bg-gray-800"
-                    : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-teal-500 block pl-3 pr-4 py-2 dark:hover:bg-gray-700 border-l-4 text-base font-medium dark:text-white"
-                } `}
-              >
-                Hire Me
               </Link>
             </div>
           </Disclosure.Panel>
